56e64a0c1c4f718c661c0f7e3f1321c82676203b
thedeepakmaurya <mauryadeepak135@gmail.com>
2024-05-23 17:15:10 +0530

    watch videos added

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,18 +1,31 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { closedMenu } from '../utils/appSlice'
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closedMenu } from "../utils/appSlice";
+import { useSearchParams } from "react-router-dom";
 
 const WatchPage = () => {
+    const [searchParams] = useSearchParams();
 
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(closedMenu())
-    }, [])
+        dispatch(closedMenu());
+    }, []);
 
     return (
-        <div>WatchPage</div>
-    )
-}
+        <div className="px-5">
+            <iframe
+                width="1000"
+                height="500"
+                src={"https://www.youtube.com/embed/"+ searchParams.get("v")}
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin"
+                allowfullscreen
+            ></iframe>
+        </div>
+    );
+};
 
-export default WatchPage
+export default WatchPage;
